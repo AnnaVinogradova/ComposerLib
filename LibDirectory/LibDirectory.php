b496cd1cc418c60c5489c58d006cbe3d53723b90
Anna Vinogradova <a.vinogradova@syberry.com>
2017-05-29 12:51:24 +0300

    Init library

---
+++ LibDirectory/LibDirectory.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Author: AnnaVinogradova
+ * Date: 4/29/2017
+ * Time: 10:26 AM
+ */
+
+namespace TestLib\LibDirectory;
+
+class LibDirectory
+{
+    public function __construct()
+    {
+        echo __DIR__;
+    }
+}
